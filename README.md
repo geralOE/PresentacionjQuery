1e7112b121c0a369679d9bae230af7546e029ff9
GMY <geraldine.macias@novohit.com>
2013-05-07 12:12:27 -0500

    Commit Initial: Presentacion y ejercicios con jQuery

---
+++ README.md
@@ -0,0 +1,5 @@
+Presentación y Ejercicios de jQuery(jQuery.com) usadas en el FLISOL 2013 de la Fac de Ingeniería - LIDSOL
+-Presentación en formato pdf, odt y pptx.
+-Uso de  HTML KickStart(http://www.99lime.com/elements/) para la página de ejercicios: index.html
+-Ejercicios con ejemplos de jQueryUI(jQueryUI.com)
+        
---
+++ js/misDemos.js
@@ -0,0 +1,86 @@
+$(document).ready(function() {
+    //datepicker
+    $( "#datepicker" ).datepicker();	
+
+
+
+    //dialog
+    $( "#dialog" ).dialog({
+      autoOpen: false,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener" ).click(function() {
+      $( "#dialog" ).dialog( "open" );
+    });
+    
+    
+    //autocomplete
+     var availableTags = [
+      "Racket",
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#tags" ).autocomplete({
+      source: availableTags
+    });
+    
+    //drag
+    $( "#draggable" ).draggable();
+    
+    
+    //graficas 1
+    $.jqplot.config.enablePlugins = true;
+        var s1 = [2, 6, 7, 10];
+        var ticks = ['a', 'b', 'c', 'd'];
+         
+        plot1 = $.jqplot('chart1', [s1], {
+            // Only animate if we're not using excanvas (not in IE 7 or IE 8)..
+            animate: !$.jqplot.use_excanvas,
+            seriesDefaults:{
+                renderer:$.jqplot.BarRenderer,
+                pointLabels: { show: true }
+            },
+            axes: {
+                xaxis: {
+                    renderer: $.jqplot.CategoryAxisRenderer,
+                    ticks: ticks
+                }
+            },
+            highlighter: { show: false }
+        });
+     
+        $('#chart1').bind('jqplotDataClick', 
+            function (ev, seriesIndex, pointIndex, data) {
+                $('#info1').html('series: '+seriesIndex+', point: '+pointIndex+', data: '+data);
+            }
+        );
+    
+});
